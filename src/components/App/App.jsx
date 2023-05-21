834eaed8c8cbef788f6f63f343dafa9dfe400231
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-21 12:42:55 +0300

    add style for App & Section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,23 @@ import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 // import upcomingEvents from '../../upcoming-events.json';
 import { Container } from './App.styled';
+import { Section } from 'components/Section/Section';
 
 export const App = () => {
   return (
     <Container>
-      <Profile text="Profile" />
-      <Statistics text="Statistics" />
-      <FriendList text="FriendList" />
-      <TransactionHistory text="TransactionHistory" />
+      <Section>
+        <Profile text="Profile" />
+      </Section>
+      <Section>
+        <Statistics text="Statistics" />
+      </Section>
+      <Section>
+        <FriendList text="FriendList" />
+      </Section>
+      <Section>
+        <TransactionHistory text="TransactionHistory" />
+      </Section>
     </Container>
   );
 };
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const Section = styled.div`
+  margin: 0px auto;
+  text-align: center;
+`;
