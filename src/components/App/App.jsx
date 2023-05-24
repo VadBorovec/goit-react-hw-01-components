b27f2fce52dafdce8c053e31c26bd35d3bb77832
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-05-24 12:23:05 +0300

    add data folder and styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import user from 'components/Profile/user.json';
-import data from 'components/Statistics/data.json';
-import friends from 'components/FriendList/friends.json';
-import transactions from 'components/TransactionHistory/transactions.json';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 import { Container } from './App.styled';
 import { Section } from 'components/Section/Section';
 
